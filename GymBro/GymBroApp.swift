62ca54f711903ab29fbb27756e4ef940636eb11a
Stefan Agusto Hutapea <stefanhutapea@gmail.com>
2024-07-12 13:08:49 +0700

    Initial Commit

---
+++ GymBro/GymBroApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymBroApp.swift
+//  GymBro
+//
+//  Created by Stefan Agusto Hutapea on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct GymBroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
